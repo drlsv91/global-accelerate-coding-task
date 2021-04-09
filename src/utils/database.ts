9c1e1dc4a28f35f114c40ae0d68da5e4adf26860
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2021-04-09 15:27:53 +0100

    config folder

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,12 @@
 import { Sequelize } from 'sequelize'
+import config from 'config'
 
+console.log(config.get('db_username'))
+console.log(config.get('db_password'))
 const sequelize = new Sequelize(
   'global-accelerate-task',
-  'root',
-  'seunlsv1992',
+  config.get('db_username'),
+  config.get('db_password'),
   { dialect: 'mysql', host: 'localhost' },
 )
 export default sequelize
